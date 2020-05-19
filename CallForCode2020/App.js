21004ec5924f92f33bd10407fffa0089891a3ec0
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-05-19 11:15:55 +0900

    expo 업데이트 + main page button 2개 생성

--- CallForCode2020/App.js
+++ CallForCode2020/App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View,Button ,Alert} from 'react-native';
 
 import useCachedResources from './hooks/useCachedResources';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
@@ -17,13 +17,22 @@ export default function App(props) {
   } else {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <View style={styles.testBtn}>
+          <Button 
+            title="News Pages"
+            color="orange"
+            onPress={() => Alert.alert('Simple Button pressed')}
+          />
+        </View>
+        <View style={styles.testBtn}>
+          <Button
+            title="Identify?"
+            color="blue"
+            onPress={() => Alert.alert('Simple Button pressed')}
+          />
+        </View>
       </View>
+      
     );
   }
 }
@@ -33,4 +42,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  testBtn: {
+        width: 107,
+        height: 40,
+        padding: 10,
+        margin:10,
+        color:"orange",
+  },
 });
+
