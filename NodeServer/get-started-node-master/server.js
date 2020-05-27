d2100294bc1493e4a3ed13887610c22dedcc2cba
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-05-27 12:40:21 +0900

    Create DB 기능 테스트

--- NodeServer/get-started-node-master/server.js
+++ NodeServer/get-started-node-master/server.js
@@ -8,7 +8,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 let mydb, cloudant;
-var vendor;
 
 var dbName = 'mydb';
 
@@ -29,7 +28,16 @@ insertOne.cloudant = function(doc, response) {
 
 getAll.cloudant = function(response) {
   var names = [];
+  mydb.get('aaa', function(err, data) {
+    console.log('Error:', err);
+    console.log('Data:', data);
+    // keep a copy of the doc so you know its revision token
+    doc = data;
+    //callback(err, data);
+  });
+  /*
   mydb.list({ include_docs: true }, function(err, body) {
+    console.log(body.rows[0].doc)
     if (!err) {
       body.rows.forEach(function(row) {
         if(row.doc.name)
@@ -37,7 +45,7 @@ getAll.cloudant = function(response) {
       });
       response.json(names);
     }
-  });
+  });*/
   //return names;
 }
 
@@ -45,13 +53,13 @@ let collectionName = 'mycollection'; // MongoDB requires a collection name.
 
 app.post("/api/visitors", function (request, response) {
   var userName = request.body.name;
-  var doc = { "name" : userName };
+  var doc = { "name" : userName , "description" : "qweqweqweqwveqwevqwe" , "test" : "test" };
   if(!mydb) {
     console.log("No database.");
     response.send(doc);
     return;
   }
-  insertOne[vendor](doc, response);
+  insertOne['cloudant'](doc, response);
 });
 
 app.get("/api/visitors", function (request, response) {
@@ -60,7 +68,7 @@ app.get("/api/visitors", function (request, response) {
     response.json(names);
     return;
   }
-  getAll[vendor](response);
+  getAll['cloudant'](response);
 });
 
 app.get("/api/test",function(req,res){
@@ -72,21 +80,19 @@ var Cloudant = require('@cloudant/cloudant');
 cloudant = Cloudant("https://0e629378-636f-46ff-8639-14e1cc9b43f7-bluemix:c76f90f2ada09c44789d322f89e5f9249632487f5bacae039eecebadd3b0c412@0e629378-636f-46ff-8639-14e1cc9b43f7-bluemix.cloudantnosqldb.appdomain.cloud");
 if(cloudant) {
   //database name
-  dbName = 'mydb';
+  dbName = 'aaa';
   // Create a new "mydb" database.
+
   cloudant.db.create(dbName, function(err, data) {
     if(!err) //err if database doesn't already exists
       console.log("Created database: " + dbName);
   });
-/*
-  cloudant.db.delete(dbName,function(err,data){
-    if(!err)
-    console.log("지우기")
-  });*/
-
-  // Specify the database we are going to use (mydb)...
   mydb = cloudant.db.use(dbName);
-  vendor = 'cloudant';
+
+  mydb.insert({ _id: 'mydoc', a: 1, b: 'two' }, function(err, data) {
+      console.log('Error:', err);
+      console.log('Data:', data);
+    });
 }
 
 //serve static file (index.html, images, css)
